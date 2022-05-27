b0dd5e97126c67b4fa5f8e27787a0418a14c066e
Albert Garuda <algarud@gmail.com>
2022-05-27 16:10:59 +0300

    hardhat config

--- chain/hardhat.config.js
+++ chain/hardhat.config.js
@@ -17,5 +17,20 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.0",
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    mumbai: {
+      url: process.env.MUMBAI_URL || "",
+      accounts:
+        process.env.MUMBAI_KEY !== undefined ? [process.env.MUMBAI_KEY] : [],
+    },
+  },
 };
