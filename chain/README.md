ee0379fe3af679eb7d76475d265908b1edb0d4c1
Albert Garuda <algarud@gmail.com>
2022-06-21 23:36:57 +0300

    faucets generation for testing

--- chain/README.md
+++ chain/README.md
@@ -3,11 +3,15 @@
 Try running some of the following tasks:
 
 ```shell
+npx hardhat node
+npx hardhat run ./scripts/deploy.js --network localhost
+npx hardhat --network localhost faucet 0x6148a120673A16e3f7BeAaAa2Fe5fa24B0803fC
+
+
 npx hardhat accounts
 npx hardhat compile
 npx hardhat clean
 npx hardhat test
-npx hardhat node
 node scripts/sample-script.js
 npx hardhat help
 ```
--- chain/hardhat.config.js
+++ chain/hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 
+const fs = require("fs");
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -19,6 +21,49 @@ task("mine", "Mines blocks")
   for (let i=0; i<b; i++) await ethers.provider.send("evm_mine"); // mines 5 blocks
   console.log("End block: ", await ethers.provider.send("eth_blockNumber"));
 });
+
+
+task("faucet", "Sends ETH and tokens to an address")
+  .addPositionalParam("receiver", "The address that will receive them")
+  .setAction(async ({ receiver }, { ethers }) => {
+    if (network.name === "hardhat") {
+      console.warn(
+        "You are running the faucet task with Hardhat network, which" +
+          "gets automatically created and destroyed every time. Use the Hardhat" +
+          " option '--network localhost'"
+      );
+    }
+
+    const addressesFile =
+      __dirname + "/../frontend/src/contracts/contract-address.json";
+
+    if (!fs.existsSync(addressesFile)) {
+      console.error("You need to deploy your contract first");
+      return;
+    }
+
+    const addressJson = fs.readFileSync(addressesFile);
+    const address = JSON.parse(addressJson);
+
+    if ((await ethers.provider.getCode(address.Token)) === "0x") {
+      console.error("You need to deploy your contract first");
+      return;
+    }
+
+    const token = await ethers.getContractAt("Token", address.Token);
+    const [sender] = await ethers.getSigners();
+
+    const tx = await token.transfer(receiver, 100);
+    await tx.wait();
+
+    const tx2 = await sender.sendTransaction({
+      to: receiver,
+      value: ethers.constants.WeiPerEther,
+    });
+    await tx2.wait();
+
+    console.log(`Transferred 1 ETH and 100 tokens to ${receiver}`);
+  });
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
