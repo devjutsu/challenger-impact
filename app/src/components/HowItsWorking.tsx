75d058ad8f4114343b0f187556a6f482b115b4aa
Albert Garuda <algarud@gmail.com>
2022-06-22 23:19:40 +0300

    how it is working - grid

--- app/src/components/HowItsWorking.tsx
+++ app/src/components/HowItsWorking.tsx
@@ -1,5 +1,34 @@
+import { Grid } from "@mui/material"
+
 export const HowItsWorking = () => {
     return (<div className='App-howitsworking'>
-        How it is working...
+        <div style={{margin:'1rem'}}><code>How it is working...</code></div>
+        <Grid container spacing={2}>
+            <Grid item xs={6}>
+                <div className='Howitsworking-block' style={{ backgroundColor: 'lime', opacity: '.4' }}>
+                    Login with your MetaMask
+                </div>
+            </Grid>
+            <Grid item xs={6}>
+                <div className='Howitsworking-block' style={{ backgroundColor: 'magenta', opacity: '.5' }}>
+                    Accept the Game
+                </div>
+            </Grid>
+            <Grid item xs={4}>
+                <div className='Howitsworking-block' style={{ backgroundColor: 'darkcyan', opacity: '.7' }}>
+                    Submit your results
+                </div>
+            </Grid>
+            <Grid item xs={4}>
+                <div className='Howitsworking-block' style={{ backgroundColor: 'orangered', opacity: '.4' }}>
+                    ...
+                </div>
+            </Grid>
+            <Grid item xs={4}>
+                <div className='Howitsworking-block' style={{ backgroundColor: 'royalblue', opacity: '.4' }}>
+                    Profit!
+                </div>
+            </Grid>
+        </Grid>
     </div>)
 }
