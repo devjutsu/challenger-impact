a31a89c62222cbc597bf25854f57a6c010252fa6
Albert Garuda <algarud@gmail.com>
2022-06-22 23:38:52 +0300

    in dev, load contract addresses from files

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,13 +1,17 @@
 import challengeAddress from './utils/Challenge.address.json';
+import daoAddress from './utils/VowDAO.address.json';
 
 export const ENVIRONMENT = process.env.APP_ENV || 'dev';
 let challengeContract = '';
+let daoContract = '';
 if (ENVIRONMENT == 'dev' || ENVIRONMENT == 'local') {
     challengeContract = challengeAddress.address;
+    daoContract = daoAddress.address;
 
 } else {
     challengeContract = process.env.REACT_APP_DAO_CONTRACT || '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+    daoContract = process.env.REACT_APP_VOW_CONTRACT || '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
 }
 
 export const REACT_APP_DAO_CONTRACT = challengeContract;
-export const REACT_APP_VOW_CONTRACT = process.env.REACT_APP_VOW_CONTRACT || '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
+export const REACT_APP_VOW_CONTRACT = daoContract;
